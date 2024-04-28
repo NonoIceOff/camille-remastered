94d17f769544403732d9703d4f206181d2dd5d07
NonoIceOff <54319211+NonoIceOff@users.noreply.github.com>
2024-04-28 16:22:23 +0200

    Modification du bot : AVRIL 2024

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    TOKEN: "MTAwNTYxODc4MzUwMDY0NDM3Mg.GMrJXf.heRpx7epBr2SWZzAkbG4rRt6vIO_UfGa9LvurA",
-    clientId: "1005618783500644372",
-    guildId : "1158387984253599826"
-}
